c1f9b3916a80eeed8e6d70109dea7d84da89bb6a
Blake VandeMerwe <blakev@null.net>
2014-09-26 23:02:12 -0600

    Modulized Falit.js

--- src/falit_b.js
+++ src/falit_b.js
@@ -1,223 +1,227 @@
-var _ = require('underscore');
+(function(module) {
+    var _ = require('underscore');
 
-var paramTypes = [
-    ['any', function(a) { return true; }],
-    ['args', _.isArguments],
-    ['array', _.isArray],
-    ['bool', _.isBoolean],
-    ['char', isChar],
-    ['date', _.isDate],
-    ['element', _.isElement],
-    ['finite', _.isFinite],
-    ['float', isFloat],
-    ['func', _.isFunction],
-    ['hex', isHex],
-    ['int', isInt],
-    ['nan', _.isNaN],
-    ['null', _.isNull],
-    ['num', _.isNumber],
-    ['obj', function(a){ return _.isObject(a) && !_.isFunction(a) && !_.isArray(a); }],
-    ['regex', _.isRegExp],
-    ['str', _.isString],
-    ['undefined', _.isUndefined]
-]
+    var paramTypes = [
+        ['any', function(a) { return true; }],
+        ['args', _.isArguments],
+        ['array', _.isArray],
+        ['bool', _.isBoolean],
+        ['char', isChar],
+        ['date', _.isDate],
+        ['element', _.isElement],
+        ['finite', _.isFinite],
+        ['float', isFloat],
+        ['func', _.isFunction],
+        ['hex', isHex],
+        ['int', isInt],
+        ['nan', _.isNaN],
+        ['null', _.isNull],
+        ['num', _.isNumber],
+        ['obj', function(a){ return _.isObject(a) && !_.isFunction(a) && !_.isArray(a); }],
+        ['regex', _.isRegExp],
+        ['str', _.isString],
+        ['undefined', _.isUndefined]
+    ]
 
-function alwaysTrue() {
-    return true;
-}
+    function alwaysTrue() {
+        return true;
+    }
 
-function isFloat(x) {
-   return typeof x === 'number' && isFinite(x) && x % 1 >= 0;
-}
+    function isFloat(x) {
+       return typeof x === 'number' && isFinite(x) && x % 1 >= 0;
+    }
 
-function isInt(x) {
-    return typeof x === 'number' && isFinite(x) && x % 1 === 0;
-}
+    function isInt(x) {
+        return typeof x === 'number' && isFinite(x) && x % 1 === 0;
+    }
 
-function isChar(x) {
-    return typeof x === 'string' && x.length == 1;
-}
+    function isChar(x) {
+        return typeof x === 'string' && x.length == 1;
+    }
 
-function isHex(x) {
-    return typeof x === 'string' && x.slice(0, 2).toLowerCase() == '0x' && function(hexVal) {
-        return hexVal.length === _.filter(hexVal.split('')
-            , function(v) { 
-                return '0123456789abcdefABCDEF'.indexOf(v) !== -1; 
-            }).length;
-    }(x.slice(2));
-}
+    function isHex(x) {
+        return typeof x === 'string' && x.slice(0, 2).toLowerCase() == '0x' && function(hexVal) {
+            return hexVal.length === _.filter(hexVal.split('')
+                , function(v) { 
+                    return '0123456789abcdefABCDEF'.indexOf(v) !== -1; 
+                }).length;
+        }(x.slice(2));
+    }
 
-var getSignature = function(args) {
-    return _.map(args, bitMaskFor);
-}
+    var getSignature = function(args) {
+        return _.map(args, bitMaskFor);
+    }
 
-var bitMaskFor = function(value) {
-    var retMaskTypes = [],
-        retBitMask = 0;
+    var bitMaskFor = function(value) {
+        var retMaskTypes = [],
+            retBitMask = 0;
 
-    _.each(varChecksObj, function(v, key, list) {
-        if (v.func(value)) {
-            retBitMask += v.bits;
-            retMaskTypes.push(v.name);
-        }
-    })
+        _.each(varChecksObj, function(v, key, list) {
+            if (v.func(value)) {
+                retBitMask += v.bits;
+                retMaskTypes.push(v.name);
+            }
+        })
 
-    return {posTypes: retMaskTypes, bitMask: retBitMask, value: value};
-}
+        return {posTypes: retMaskTypes, bitMask: retBitMask, value: value};
+    }
 
-var varChecksObj = function() {
-    var retObj = {};
+    var varChecksObj = function() {
+        var retObj = {};
 
-    _.each(paramTypes, function(pType, index) {
-        retObj[pType[0]] = {
-            name: pType[0],
-            func: pType[1],
-            bits: Math.pow(2, index)
-        }
-    })
+        _.each(paramTypes, function(pType, index) {
+            retObj[pType[0]] = {
+                name: pType[0],
+                func: pType[1],
+                bits: Math.pow(2, index)
+            }
+        })
 
-    return retObj;
-}()
+        return retObj;
+    }()
 
-var requiredTypes = function(strict) {
-    var reqTypes = {};
+    var requiredTypes = function(strict) {
+        var reqTypes = {};
 
-    _.each(varChecksObj, function(value, key, list) {
-        reqTypes[key] = _.partial(
-            function(name, verifyFunc, bits, strict, customFunc) {
-                var defaultValue = null;
+        _.each(varChecksObj, function(value, key, list) {
+            reqTypes[key] = _.partial(
+                function(name, verifyFunc, bits, strict, customFunc) {
+                    var defaultValue = null;
 
-                if (strict) {
-                    if (!_.isFunction(customFunc)) {
-                        customFunc = alwaysTrue;
-                    }    
-                } else { // optional parameter with default
-                    if (verifyFunc(customFunc)) {
-                        defaultValue = customFunc;
+                    if (strict) {
+                        if (!_.isFunction(customFunc)) {
+                            customFunc = alwaysTrue;
+                        }    
+                    } else { // optional parameter with default
+                        if (verifyFunc(customFunc)) {
+                            defaultValue = customFunc;
+                        }
                     }
-                }
-                
+                    
+                    if (typeof defaultValue === 'undefined') {
+                        defaultValue = null;
+                    }
 
-                return {    // return an object so we know if the type has been primed
-                    name: name,
-                    required: strict,
-                    defaultValue: defaultValue, 
-                    parse: function(argsPos, newValue) 
-                    {
-                        var customFuncMemo = (this.required) ? customFunc(newValue) : true,
-                            verifyFuncMemo = verifyFunc(newValue);
+                    return {    // return an object so we know if the type has been primed
+                        name: name,
+                        required: strict,
+                        defaultValue: defaultValue, 
+                        parse: function(argsPos, newValue) 
+                        {
+                            var customFuncMemo = (this.required) ? customFunc(newValue) : true,
+                                verifyFuncMemo = verifyFunc(newValue);
 
-                        if (verifyFuncMemo && customFuncMemo) {
-                            return {defValue: false, value: newValue, passed: verifyFuncMemo};
-                        } else {
-                            var received = bitMaskFor(newValue);
-
-                            if (!customFuncMemo && verifyFuncMemo) {
-                                var estr = [
-                                    'Type validateFunction error!',
-                                    'Supplied value did not pass validation.',
-                                    'Expected "' + name + '" in position_' + argsPos + ' to pass.'
-                                ]
+                            if (verifyFuncMemo && customFuncMemo) {
+                                return {defValue: false, value: newValue, passed: verifyFuncMemo};
                             } else {
-                                var estr = [
-                                    'Invalid type supplied!',
-                                    'Expected [' + name + '] in position_' + argsPos + '.',
-                                    'Received: [' + received.posTypes + '], with value: ' + received.value
-                                ]
-                            }
+                                var received = bitMaskFor(newValue);
 
-                            if (this.required) {
-                                throw Error(estr.join('\n'));    
-                            } else {
-                                return {defValue: true, value: defaultValue, passed: verifyFuncMemo};
+                                if (!customFuncMemo && verifyFuncMemo) {
+                                    var estr = [
+                                        'Type validateFunction error!',
+                                        'Supplied value did not pass validation.',
+                                        'Expected "' + name + '" in position_' + argsPos + ' to pass.'
+                                    ]
+                                } else {
+                                    var estr = [
+                                        'Invalid type supplied!',
+                                        'Expected [' + name + '] in position_' + argsPos + '.',
+                                        'Received: [' + received.posTypes + '], with value: ' + received.value
+                                    ]
+                                }
+
+                                if (this.required) {
+                                    throw Error(estr.join('\n'));    
+                                } else {
+                                    return {defValue: true, value: defaultValue, passed: verifyFuncMemo};
+                                }
                             }
                         }
                     }
-                }
-            }, value.name, value.func, value.bits, strict
-        );
-    });
+                }, value.name, value.func, value.bits, strict
+            );
+        });
 
-    return reqTypes;
-}
-
-
-var binder = function() {
-    if (!_.isFunction(_.last(arguments))) {
-        throw new Error('Last argument is not a function; nothing to bind to!');
+        return reqTypes;
     }
 
-    var signature = _.initial(arguments);
 
-    return _.partial(function(bindSignatureFuncs, bindFunc) {
-        var validArgs = _.rest(arguments, 2),
-            newSignature = getSignature(validArgs),
-            argsCount = 0;
+    var binder = function() {
+        if (!_.isFunction(_.last(arguments))) {
+            throw new Error('Last argument is not a function; nothing to bind to!');
+        }
 
-        var transFuncs = _.map(bindSignatureFuncs, function(f) {
-            return (typeof f === 'function') ? f() : f;
-        })
+        var signature = _.initial(arguments);
 
-        var bSigIndex = 0, // boundSignature array index
-            passes = true;
+        return _.partial(function(bindSignatureFuncs, bindFunc) {
+            var validArgs = _.rest(arguments, 2),
+                newSignature = getSignature(validArgs),
+                argsCount = 0;
 
-        var newArgs = [];
+            var transFuncs = _.map(bindSignatureFuncs, function(f) {
+                return (typeof f === 'function') ? f() : f;
+            })
 
-        _.each(newSignature, function(f, index) {
-            if (bSigIndex > transFuncs.length - 1) {
-                return; 
-            }
+            var bSigIndex = 0, // boundSignature array index
+                passes = true;
 
-            if (transFuncs[bSigIndex].required) {
+            var newArgs = [];
 
-                var posPass = transFuncs[bSigIndex++].parse(argsCount++, f.value);
-                    newArgs.push(posPass.value);
+            _.each(newSignature, function(f, index) {
+                if (bSigIndex > transFuncs.length - 1) {
+                    return; 
+                }
 
-                passes = passes && posPass;
-
-            } else { // optional parameter
-                while (bSigIndex < transFuncs.length) {
+                if (transFuncs[bSigIndex].required) {
 
                     var posPass = transFuncs[bSigIndex++].parse(argsCount++, f.value);
-
                         newArgs.push(posPass.value);
 
-                    if (posPass.passed) {
-                        break;
+                    passes = passes && posPass;
+
+                } else { // optional parameter
+                    while (bSigIndex < transFuncs.length) {
+
+                        var posPass = transFuncs[bSigIndex++].parse(argsCount++, f.value);
+
+                            newArgs.push(posPass.value);
+
+                        if (posPass.passed) {
+                            break;
+                        }
                     }
                 }
+            })
+
+            while (bSigIndex < transFuncs.length) {
+                if (transFuncs[bSigIndex].required) {
+                    var errMsg = 'Missing required [' + transFuncs[bSigIndex].name + '] parameter at position_' + argsCount;
+                    throw new Error(errMsg);
+                } else {
+                    newArgs.push(transFuncs[bSigIndex].defaultValue);;
+                }
+
+                bSigIndex++;
             }
-        })
 
-        console.log(transFuncs)
-        console.log(newSignature)
-        console.log(bSigIndex);
-
-        while (bSigIndex < transFuncs.length) {
-            if (transFuncs[bSigIndex].required) {
-                var errMsg = 'Mising required [' + transFuncs[bSigIndex].name + '] parameter at position_' + argsCount;
-                throw new Error(errMsg);
+            if (passes) {
+                bindFunc.apply(this, newArgs);
             } else {
-                newArgs.push(transFuncs[bSigIndex].defaultValue);;
+                console.log('oh nohes! defdefdef');
+                bindFunc.apply(this, validArgs);
             }
 
-            bSigIndex++;
-            argsCount;
-        }
+        }, signature, _.last(arguments));
+    }
 
-        if (passes) {
-            bindFunc.apply(this, newArgs);
-        } else {
-            console.log('oh nohes!');
-            bindFunc.apply(this, validArgs);
-        }
+    var req = requiredTypes(true),
+        opt = requiredTypes(false);
 
-    }, signature, _.last(arguments));
-}
+    module.exports = {
+        force: binder,
+        required: req,
+        optional: opt
+    };
+})(typeof exports === 'undefined' ? this['falit'] = {} : module);
 
-var req = requiredTypes(true),
-    opt = requiredTypes(false);
 
-var test = binder(req.str, req.any, console.log)
-
-test('blake', 2);
